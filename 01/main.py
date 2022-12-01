a3417c92884bcfe41053ee14c749444c077bb0a2
Max Ries <max.ries113@gmail.com>
2022-12-01 20:55:23 +0100

    01 - Second Try

--- 01/main.py
+++ 01/main.py
@@ -3,7 +3,7 @@ max_calories = 0
 with open('./01/input.txt', 'r') as f:
     current_calories = 0
     for line in f:
-        if line == 'n':
+        if line == '\n':
             if current_calories > max_calories:
                 max_calories = current_calories
         else:
